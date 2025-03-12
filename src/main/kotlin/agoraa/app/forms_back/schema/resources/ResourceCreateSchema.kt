0a350f84edfc094fc6edad49aba3bb0fce33df7b
PedroCostaa <ph.mc@hotmail.com>
2025-03-12 09:30:37 -0300

    fix: remove constraint Pattern in ResourceCreateSchema store attribute

--- src/main/kotlin/agoraa/app/forms_back/schema/resources/ResourceCreateSchema.kt
+++ src/main/kotlin/agoraa/app/forms_back/schema/resources/ResourceCreateSchema.kt
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Pattern
 
 data class ResourceCreateSchema(
     @field:NotNull(message = "Store cannot be null")
-    @field:Pattern(regexp = "TRESMANN_SMJ|TRESMANN_VIX|TRESMANN_STT", message = "Invalid Store")
     val store: StoresEnum,
     @field:NotNull(message = "Products cannot be null")
     val products: List<ResourceProductsCreateSchema>
