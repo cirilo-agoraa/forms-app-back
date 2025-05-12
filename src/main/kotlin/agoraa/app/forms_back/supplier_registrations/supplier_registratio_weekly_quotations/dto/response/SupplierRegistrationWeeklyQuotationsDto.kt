c06f2a2a833911bbb83b6e9e090fe19019889173
PedroCostaa <ph.mc@hotmail.com>
2025-05-12 07:25:36 -0300

    fix: add include always in dtos

--- src/main/kotlin/agoraa/app/forms_back/supplier_registrations/supplier_registratio_weekly_quotations/dto/response/SupplierRegistrationWeeklyQuotationsDto.kt
+++ src/main/kotlin/agoraa/app/forms_back/supplier_registrations/supplier_registratio_weekly_quotations/dto/response/SupplierRegistrationWeeklyQuotationsDto.kt
@@ -1,6 +1,10 @@
 package agoraa.app.forms_back.supplier_registrations.supplier_registratio_weekly_quotations.dto.response
 
+import agoraa.app.forms_back.shared.enums.suppliers_registration.WeeklyQuotationEnum
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.ALWAYS)
 data class SupplierRegistrationWeeklyQuotationsDto(
     val id: Long,
-    val weeklyQuotation: agoraa.app.forms_back.shared.enums.suppliers_registration.WeeklyQuotationEnum
+    val weeklyQuotation: WeeklyQuotationEnum
 )
--- src/main/kotlin/agoraa/app/forms_back/supplier_registrations/supplier_registration_stores/dto/response/SupplierRegistrationStoresDto.kt
+++ src/main/kotlin/agoraa/app/forms_back/supplier_registrations/supplier_registration_stores/dto/response/SupplierRegistrationStoresDto.kt
@@ -1,17 +1,19 @@
 package agoraa.app.forms_back.supplier_registrations.supplier_registration_stores.dto.response
 
+import agoraa.app.forms_back.shared.enums.OrderDaysEnum
+import agoraa.app.forms_back.shared.enums.StoresEnum
 import com.fasterxml.jackson.annotation.JsonInclude
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.ALWAYS)
 data class SupplierRegistrationStoresDto(
     val id: Long,
-    val store: agoraa.app.forms_back.shared.enums.StoresEnum,
+    val store: StoresEnum,
     val deliverInStore: Boolean = false,
     val deliveryTime: Int? = null,
     val sellerName: String? = null,
     val sellerPhone: String? = null,
     val sellerEmail: String? = null,
-    val orderBestDay: agoraa.app.forms_back.shared.enums.OrderDaysEnum? = null,
+    val orderBestDay: OrderDaysEnum? = null,
     val routine: String? = null,
     val motive: String? = null,
 )
