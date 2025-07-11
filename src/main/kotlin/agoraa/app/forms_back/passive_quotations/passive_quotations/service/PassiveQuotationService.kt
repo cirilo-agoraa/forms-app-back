aa437b7ccb8f1cee1fc4601fec0cd3e8ba5b3346
cirilo55 <cirilo.andrade1@gmail.com>
2025-07-11 12:02:39 -0300

    .;

--- src/main/kotlin/agoraa/app/forms_back/passive_quotations/passive_quotations/service/PassiveQuotationService.kt
+++ src/main/kotlin/agoraa/app/forms_back/passive_quotations/passive_quotations/service/PassiveQuotationService.kt
@@ -462,7 +462,7 @@ class PassiveQuotationService(
 
         pendingQuotations.forEach { quotation ->
             val msg = "COTAÇÃO FORNECEDOR ${quotation.supplier.name} PENDENTE há mais de uma hora"
-            chatsacService.sendMsg(msg, "663a53e93b0a671bbcb23c93").subscribe()
+            chatsacService.sendMsg(msg, "663ba02dc528f6252825d821").subscribe()
         }
     }
 }
