6f978e71fc977b53fc07f7fe6c93d788a49bd4be
cirilo55 <cirilo.andrade1@gmail.com>
2025-08-12 10:33:20 -0300

    .

--- src/main/kotlin/agoraa/app/forms_back/shared/enums/WppGroupsEnum.kt
+++ src/main/kotlin/agoraa/app/forms_back/shared/enums/WppGroupsEnum.kt
@@ -3,7 +3,8 @@ package agoraa.app.forms_back.shared.enums
 enum class WppGroupsEnum(private val groupId: String) {
     LANCAMENTO_COTACAO("6749b7eb8f2a5e3014639a2c"),
     CARGA_FECHADA("66aa84a8b959011aa7277c7a"),
-    AMBIENTE_DE_TESTE("663a53e93b0a671bbcb23c93");
+    AMBIENTE_DE_TESTE("663a53e93b0a671bbcb23c93"),
+    COTACAO_ENCARTE("688d1c4b8228775238a991ab");
 
     fun getGroupId(): String {
         return groupId
